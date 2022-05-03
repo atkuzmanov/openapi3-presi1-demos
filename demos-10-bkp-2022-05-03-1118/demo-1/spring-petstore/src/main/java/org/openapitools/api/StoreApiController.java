d95602b204525be668790dbb7ea5186e5182835a
atkuzmanov <atkuzmanov@gmail.com>
2022-05-03 11:23:53 +0300

    Work-In-Progress-WIP-2022-05-03-112353

---
+++ demos-10-bkp-2022-05-03-1118/demo-1/spring-petstore/src/main/java/org/openapitools/api/StoreApiController.java
@@ -0,0 +1,27 @@
+package org.openapitools.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-05-03T10:59:52.056765+03:00[Europe/Sofia]")
+@Controller
+@RequestMapping("${openapi.openAPIPetstore.base-path:}")
+public class StoreApiController implements StoreApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public StoreApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
